fee12a0e21e404895792b67aa90de6a7b6139fec
ttkien2035 <trantrungkien2035@gmail.com>
2022-05-31 21:40:24 +0700

    create requirements file

--- init_db.py
+++ init_db.py
@@ -9,57 +9,57 @@ db = mysql.connector.connect(
 
 cursor = db.cursor()
 # account Table
-cursor.execute('''
-  CREATE TABLE IF NOT EXISTS account (
-    id INT NOT NULL AUTO_INCREMENT, 
-    email CHAR(50) NOT NULL, 
-    password CHAR(50) NOT NULL, 
-    name NVARCHAR(50), 
-    phone CHAR(50), 
-    address NVARCHAR(50), 
-    role INT NOT NULL DEFAULT 1, 
-    image MEDIUMBLOB, 
-    PRIMARY KEY (id)
-  );
-''')
+# cursor.execute('''
+#   CREATE TABLE IF NOT EXISTS account (
+#     id INT NOT NULL AUTO_INCREMENT, 
+#     email CHAR(50) NOT NULL, 
+#     password CHAR(50) NOT NULL, 
+#     name NVARCHAR(50), 
+#     phone CHAR(50), 
+#     address NVARCHAR(50), 
+#     role INT NOT NULL DEFAULT 1, 
+#     image MEDIUMBLOB, 
+#     PRIMARY KEY (id)
+#   );
+# ''')
 
-# category Table
-cursor.execute('''
-  CREATE TABLE IF NOT EXISTS category (
-    id INT NOT NULL AUTO_INCREMENT, 
-    name NVARCHAR(50), 
-    image MEDIUMBLOB,
-    PRIMARY KEY (id)
-  );
-''')
+# # category Table
+# cursor.execute('''
+#   CREATE TABLE IF NOT EXISTS category (
+#     id INT NOT NULL AUTO_INCREMENT, 
+#     name NVARCHAR(50), 
+#     image MEDIUMBLOB,
+#     PRIMARY KEY (id)
+#   );
+# ''')
 
-# product Table
-cursor.execute('''
-  CREATE TABLE IF NOT EXISTS product (
-    id INT NOT NULL AUTO_INCREMENT, 
-    name NVARCHAR(50) NOT NULL, 
-    description NVARCHAR(256), 
-    price INT, 
-    quantity INT, 
-    image MEDIUMBLOB, 
-    id_category INT NOT NULL, 
-    PRIMARY KEY (id),
-    FOREIGN KEY (id_category) REFERENCES category(id)
-  );
-''')
+# # product Table
+# cursor.execute('''
+#   CREATE TABLE IF NOT EXISTS product (
+#     id INT NOT NULL AUTO_INCREMENT, 
+#     name NVARCHAR(50) NOT NULL, 
+#     description NVARCHAR(256), 
+#     price INT, 
+#     quantity INT, 
+#     image MEDIUMBLOB, 
+#     id_category INT NOT NULL, 
+#     PRIMARY KEY (id),
+#     FOREIGN KEY (id_category) REFERENCES category(id)
+#   );
+# ''')
 
-# cart Table
-cursor.execute('''
-  CREATE TABLE IF NOT EXISTS cart (
-    id INT NOT NULL AUTO_INCREMENT, 
-    id_account INT NOT NULL,
-    id_product INT NOT NULL,
-    amount_product INT DEFAULT 0,
-    PRIMARY KEY (id),
-    FOREIGN KEY (id_account) REFERENCES account(id),
-    FOREIGN KEY (id_product) REFERENCES product(id)
-  );
-''')
+# # cart Table
+# cursor.execute('''
+#   CREATE TABLE IF NOT EXISTS cart (
+#     id INT NOT NULL AUTO_INCREMENT, 
+#     id_account INT NOT NULL,
+#     id_product INT NOT NULL,
+#     amount_product INT DEFAULT 0,
+#     PRIMARY KEY (id),
+#     FOREIGN KEY (id_account) REFERENCES account(id),
+#     FOREIGN KEY (id_product) REFERENCES product(id)
+#   );
+# ''')
 # Bill Table
 cursor.execute('''
   CREATE TABLE IF NOT EXISTS bill (
